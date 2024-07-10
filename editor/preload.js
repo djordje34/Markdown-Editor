4ba7ffda160ccc73a0ed3a03fbd900406dd93741
djordje34 <djordje00karisic@gmail.com>
2024-07-10 20:39:49 +0200

    Create preload.js

---
+++ editor/preload.js
@@ -0,0 +1,16 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  send: (channel, data) => {
+    const validChannels = ['save-content', 'get-directory-structure', 'open-file-from-tree'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    const validChannels = ['save-file', 'open-file', 'directory-structure'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  }
+});
